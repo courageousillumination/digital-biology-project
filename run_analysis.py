fa06e45445ea1939099a4da2497c38ddc71a2b7d
Tristan Rasmussen <tristanr93@gmail.com>
2015-05-17 11:41:31 -0500

    Adding histogram of distances.

--- run_analysis.py
+++ run_analysis.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     # for pdb_file in find_pdb_files(sys.argv[1]):
     #     data = parse_pdb(pdb_file)
     #     phospo_sites = data.get_phoso_sites()
-        # for x in of_interest:
-        #     counts[x] += len(data.get_compounds([x]))
+    #     for x in of_interest:
+    #         counts[x] += len(data.get_compounds([x]))
         # print("{} has {} phosphorylation sites (types: {})".format(pdb_file, len(phospo_sites), list(set([x.name for x in phospo_sites]))))
         # counts.append(len(phospo_sites))
     # print counts
@@ -26,19 +26,28 @@ if __name__ == "__main__":
     # pyplot.xlabel("Number of phosphorylation sites")
     # pyplot.ylabel("Number of occurances")
     # pyplot.savefig("histogram.png")
+    values = []
     dehydrons = get_dehydrons("wrappers.txt")
     data = parse_pdb("./1a81H.pdb")
     phospo_sites = data.get_phoso_sites()
-    print dehydrons
+    non_phoso_sites = data.get_compounds(names=["TYR", "THR", "SER"])
     for dehydron in dehydrons:
         residue1 = data.get_residue_by_id(dehydron[0][0], dehydron[0][1])
         residue2 = data.get_residue_by_id(dehydron[1][0], dehydron[1][1])
         position1 = residue1.get_atom("CA").position
         position2 = residue2.get_atom("CA").position
-        for phospo_site in phospo_sites:
-            phospo_position = phospo_site.get_atom("CA").position
+        for site in non_phoso_sites:
+        #for site in phospo_sites:
+            phospo_position = site.get_atom("CA").position
             distance1 = distance(position1, phospo_position)
             distance2 = distance(position2, phospo_position)
-            if distance1 < 6.5 and distance2 < 6.5:
-                print(phospo_site, dehydron, distance1, distance2)
-    # print data, dehydrons
+            values.append(distance1 + distance2 / 2.0)
+            # if distance1 < 6.5 and distance2 < 6.5:
+            #     print(site, dehydron, distance1, distance2)
+    #print data, dehydrons
+    pyplot.hist(values, 25, normed=True)
+    pyplot.xlim(0, 180)
+    pyplot.xlabel("Average distance (A)")
+    pyplot.ylabel("Probability")
+    pyplot.title("Distribution of distances for Non-Phosphorylated residues")
+    pyplot.savefig("non_phospho.png")
